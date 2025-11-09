c3a3b8bdbbe27faf605525bbe3bd4bf225e81b61
Aniket Shirsath <aniketshirsath1411@gmail.com>
2025-11-09 11:30:50 +0530

    ok

--- src/Components/Bni.jsx
+++ src/Components/Bni.jsx
@@ -6,7 +6,7 @@ const Bni = () => {
     return (
         <div>
             <div className="bni-badge">
-                <img src="./img/BNI_logo.png" alt="BNI" />
+                <img src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/WHB2G7Z3SOWCR7uiVrxs/media/a3bbc200-f631-4798-8aa4-387b4193e1e7.png" alt="BNI" />
                 <span>Proud Member of BNI</span>
             </div>
         </div>
